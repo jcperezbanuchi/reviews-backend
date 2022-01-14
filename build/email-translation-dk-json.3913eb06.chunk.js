89e1aec441dfe4f26f475dbbd2f945d4c1a4482c
Juan C Perez Banuchi <juanco1935@gmail.com>
2022-01-14 12:45:32 -0400

    added categories to the reviews

---
+++ build/email-translation-dk-json.3913eb06.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbackend=self.webpackChunkbackend||[]).push([[3825],{42676:e=>{e.exports={}}}]);
---
+++ build/email-translation-es-json.14758678.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbackend=self.webpackChunkbackend||[]).push([[5751],{10365:e=>{e.exports={}}}]);
